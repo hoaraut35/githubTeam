b99d6179ab735edfe5f0e2e1f436b4114fde40ff
Thierry Bitar <bitarthierry@gmail.com>
2022-02-02 11:13:29 +0100

    third Edit

--- app/src/main/java/com/testmyteam/githubteam/MainActivity.kt
+++ app/src/main/java/com/testmyteam/githubteam/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val firstEdit = 1
         val secondEdit = 2
+        val thirdCommit = 3
     }
 }
